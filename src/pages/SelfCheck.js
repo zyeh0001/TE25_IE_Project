43f8f8cd8465ce476225b3c95637da46d548d343
MinuetW <rafa0730@hotmail.com>
2021-08-23 16:41:18 +1000

    image-uploading

--- src/pages/SelfCheck.js
+++ src/pages/SelfCheck.js
@@ -1,4 +1,10 @@
+import ImageUploading from "../components/ImageUploading";
+
 function SelfCheckPage() {
-  return <div>self check page</div>;
+  return <div>self check page
+    <div id="ImageUpload">
+      <ImageUploading />
+    </div>
+  </div>;
 }
 export default SelfCheckPage;
